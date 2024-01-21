a50dd5b41f7de171bd9af31a423ebb72e32d4627
sara-aref <147546807+sara-aref@users.noreply.github.com>
2024-01-21 20:29:11 +0200

    Add files via upload

---
+++ lab1/Ex1-2/check.java
@@ -0,0 +1,10 @@
+class check {
+	public static void main(String args[])
+	{
+		int intvalue = Integer.parseInt(args[0]);
+		for (int i=0; i<intvalue; i++)
+		{
+			System.out.println(args[1]);
+		}
+	}
+}
---
+++ lab1/Ex1-2/readfile.java
@@ -0,0 +1,16 @@
+class readfile {
+	public static void main(String args[])
+	{
+		int value = Integer.parseInt(args[0]);
+		if (value % 2 == 0)
+		{
+			System.out.println("even");
+		}
+		else
+		{
+			System.out.println("odd");
+		}
+		
+	}
+}
+
